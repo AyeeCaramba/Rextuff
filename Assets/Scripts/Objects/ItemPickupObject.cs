eb806cd7d7ebb4744fc0e7d493db401c87296705
Justin T Hamm <justin.t.hamm@gmail.com>
2016-11-10 08:39:32 -0800

    All the progress so far.
    It's not quite ready, don't bitch if it doesn't work or lacks comments.
    I'm not feeling well.

---
+++ Assets/Scripts/Objects/ItemPickupObject.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickupObject : CoreObject
+{
+
+    private float currentRespawnTime;
+    public float respawnTime;
+
+    public string accessory;
+
+    public string weapon;
+
+	// Use this for initialization
+	protected override void Start () {
+        base.Start();
+        currentRespawnTime = respawnTime;
+	}
+	
+	// Update is called once per frame
+	protected override void Update () {
+        base.Update();
+        currentRespawnTime += Time.deltaTime;
+	}
+
+    protected override void OnPlayerCollision(PlayerController player)
+    {
+        base.OnPlayerCollision(player);
+        if (currentRespawnTime >= respawnTime && player.motor is BipedMotor)
+        {
+            if (accessory != null)
+            {
+                ((BipedStats)player.stats).accessory = accessory;
+            }
+            if(weapon != null)
+            {
+                ((BipedStats)player.stats).weapon = player.gameObject.AddComponent<Types.GetType(weapon, );
+            }
+
+            respawnTime = 0;
+        }
+    }
+}
